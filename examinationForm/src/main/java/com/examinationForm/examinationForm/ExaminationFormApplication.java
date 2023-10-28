93c3e7d1cc7da24208dc2cd670dc4dcaac3d5940
Ayeni Emmanuel Olakunle <ayeniolakunle98@gmail.com>
2023-10-28 15:14:26 +0100

    first commit

---
+++ examinationForm/src/main/java/com/examinationForm/examinationForm/ExaminationFormApplication.java
@@ -0,0 +1,13 @@
+package com.examinationForm.examinationForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationFormApplication.class, args);
+	}
+
+}
---
+++ examinationForm/src/test/java/com/examinationForm/examinationForm/ExaminationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examinationForm.examinationForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
